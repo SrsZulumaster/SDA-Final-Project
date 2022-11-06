4eda27b344c86d6561833536c189de706e2e3e89
SrsZulumaster <minaolenzimmer@gmail.com>
2022-11-06 13:49:07 +0200

    added requirements.txt

--- SDAFinal/SmartApp/forms.py
+++ SDAFinal/SmartApp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class InputForm(forms.Form):
-    text_input = forms.CharField(label="text_input_area")
+    text_input = forms.CharField(label="text_input", widget=forms.TextInput())
     switch1 = forms.BooleanField(required=False)
     switch2 = forms.BooleanField(required=False)
     switch3 = forms.BooleanField(required=False)
--- SDAFinal/SmartApp/views.py
+++ SDAFinal/SmartApp/views.py
@@ -11,12 +11,15 @@ def index(request):
         form = InputForm(request.POST)
         if form.is_valid():
             text_input = form.text_input
-            if form.switch1== "on":
-                return output(request)
+            if form.switch1 == "on":
+                stringy= "asdad"
+                stringy= stringy.upper()
+                text_input = text_input.upper()
+                return output(request, text_input)
     else:
         form = InputForm()
     return render(request, "text_input.html", {"form": form})
 
 
-def output(request,text):
+def output(request, text):
     return render(request, template_name="text-output.html")
