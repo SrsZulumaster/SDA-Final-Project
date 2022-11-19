9a054d6e8295901b5c4b383ab9119bc39ff978ba
SrsZulumaster <minaolenzimmer@gmail.com>
2022-11-19 13:43:16 +0200

    both input and output work, need styling

--- SDAFinal/SmartApp/forms.py
+++ SDAFinal/SmartApp/forms.py
@@ -1,11 +1,26 @@
 from django import forms
+from spellchecker import SpellChecker
+import wikipedia
 
 
 class InputForm(forms.Form):
-    text_input = forms.CharField(label="text_input", widget=forms.TextInput())
-    switch1 = forms.BooleanField(required=False)
-    switch2 = forms.BooleanField(required=False)
-    switch3 = forms.BooleanField(required=False)
-    switch4 = forms.BooleanField(required=False)
-    switch5 = forms.BooleanField(required=False)
-    switch6 = forms.BooleanField(required=False)
+    text_input = forms.CharField(widget=forms.Textarea(
+        attrs={"class": "form-control",
+               "rows": "6"
+                }))
+    punctuations = forms.BooleanField(required=False, widget=)
+    upper = forms.BooleanField(required=False)
+    lower = forms.BooleanField(required=False)
+    removeLine = forms.BooleanField(required=False)
+    removeSpace = forms.BooleanField(required=False)
+    countChars = forms.BooleanField(required=False)
+    spellCheck = forms.BooleanField(required=False)
+    summary = forms.BooleanField(required=False)
+    removeStop = forms.BooleanField(required=False)
+
+    def __str__(self):
+        return self.text_input
+
+
+class OutputForm(forms.Form):
+    text_output = forms.Textarea()
--- SDAFinal/SmartApp/views.py
+++ SDAFinal/SmartApp/views.py
@@ -1,25 +1,109 @@
+import wikipedia
 from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
-from SmartApp.forms import InputForm
+from spellchecker import SpellChecker
+
+from SmartApp.forms import InputForm, OutputForm
 
 
 def index(request):
-    if request.method == "POST":
-        form = InputForm(request.POST)
+    if request.method == "GET":
+        form = InputForm(request.GET)
         if form.is_valid():
-            text_input = form.text_input
-            if form.switch1 == "on":
-                stringy= "asdad"
-                stringy= stringy.upper()
-                text_input = text_input.upper()
-                return output(request, text_input)
+            return output(request)
     else:
         form = InputForm()
     return render(request, "text_input.html", {"form": form})
 
 
-def output(request, text):
-    return render(request, template_name="text-output.html")
+def output(request):
+    content = request.GET.get('text_input')
+    punctuations = request.GET.get('punctuations')
+    upper = request.GET.get('upper')
+    lower = request.GET.get('lower')
+    removespace = request.GET.get("removeSpace")
+    removeline = request.GET.get("spellCheck")
+    charAmount = request.GET.get('countChars')
+    summary = request.GET.get('summary')
+    removeStop = request.GET.get('removeStop')
+    if punctuations == "on":
+        return render(request, "text-output.html", {"form": punctuationsRemove(content),
+                                                    "head": "Text Without Punctuations"})
+    elif upper == "on":
+        return render(request, "text-output.html", {"form": makeUpper(content),
+                                                    "head": "Text In Capitals"})
+    elif lower == "on":
+        return render(request, "text-output.html", {"form": makeLower(content),
+                                                    "head": "Text in Lowercase"})
+    elif removespace == "on":
+        return render(request, "text-output.html", {"form": removeExtraSpace(content),
+                                                    "head": "Text with no extra spaces"})
+    elif removeline == "on":
+        return render(request, "text-output.html", {"form": removeNewLine(content),
+                                                    "head": "Text without new line"})
+    elif charAmount == "on":
+        return render(request, "text-output.html", {"form": countCharAmount(content),
+                                                    "head": "Your Text Length was:"})
+    elif summary == "on":
+        return render(request, "text-output.html", {"form": makeSummary(content),
+                                                    "head": "Your Word Summary is:"})
+    elif removeStop == "on":
+        return render(request, "text-output.html", {"form": removeWordSTOP(content),
+                                                    "head": "Your text without the word Stop"})
+    else:
+        return render(request, "text-output.html", {"form": content,
+                                                    "head": "Your original text:"})
+
+
+def punctuationsRemove(text_input: str):
+    text_output = text_input.replace(".", "")
+    return text_output
+
+
+def makeUpper(text_input: str):
+    text_output = text_input.upper()
+    return text_output
+
+
+def makeLower(text_input: str):
+    text_output = text_input.lower()
+    return text_output
+
+
+def removeNewLine(text_input: str):
+    text_output = text_input.strip()
+    return text_output
+
+
+def removeExtraSpace(text_input: str):
+    while '  ' in text_input:
+        text_input = text_input.replace('  ', ' ')
+
+    text_output = text_input
+    return text_output
+
+
+def countCharAmount(text_input: str):
+    text_output = len(text_input)
+    return text_output
+
+
+def checkSpelling(text_input: str):
+    spell = SpellChecker()
+    text_output = spell.unknown(text_input)
+    return text_output
+
+
+def makeSummary(text_input: str):
+    text_output = wikipedia.summary(text_input)
+    return text_output
+
+
+def removeWordSTOP(text_input: str):
+    text_input = text_input.replace("stop", "")
+    text_input = text_input.replace("Stop", "")
+    text_output = text_input.replace("STOP", "")
+    return text_output
